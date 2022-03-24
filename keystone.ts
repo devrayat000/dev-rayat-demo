37275d21bdc75e66f3853ea02656d0a921f39128
devrayat000 <2010050@me.buet.ac.bd>
2022-03-24 15:58:13 +0600

    my setup

--- keystone.ts
+++ keystone.ts
@@ -20,3 +20,14 @@ export default withAuth(
     session,
   })
 );
+
+// storage-account-name=demostoragerayat
+// storage-key=iTkZ3TVVlanYGtDKemaOAeVfyR7mFld5id2jlS6a5U83PNuLWQmzP+PQ2svJczxm0y7QKDmyCTGw+AStFTrM6g==
+
+// db-server=demo-dev-db.postgres.database.azure.com
+// db-admin=demo@demo-dev-db
+// db-url=postgres://demo@demo-dev-db:ppooii12!@demo-dev-db.postgres.database.azure.com:5432/portfolio
+// db-cli=psql --host demo-dev-db.postgres.database.azure.com --user demo@demo-dev-db --port=5432 --dbname postgres
+
+// deploy=https://camo.githubusercontent.com/bad3d579584bd4996af60a96735a0fdcb9f402933c139cc6c4c4a4577576411f/68747470733a2f2f616b612e6d732f6465706c6f79746f617a757265627574746f6e
+// https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Faaronpowell%2Fkeystone-6-azure-example%2Fmain%2F.azure%2Fazuredeploy.json
